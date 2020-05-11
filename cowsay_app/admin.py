c610f9071049066f6812b8ead11c2db2816288d4
luisfff29 <luisfff29@gmail.com>
2020-05-11 10:33:21 -0500

    add models

--- cowsay_app/admin.py
+++ cowsay_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from cowsay_app.models import Main
 
 # Register your models here.
+admin.site.register(Main)
--- cowsay_app/models.py
+++ cowsay_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Main(models.Model):
+    text = models.CharField()
+
+    def __str__(self):
+        return self.text
