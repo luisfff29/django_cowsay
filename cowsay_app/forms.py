4b564fd76fd2f844d0e891316503e46750cb7888
luisfff29 <luisfff29@gmail.com>
2020-05-12 22:22:57 -0500

    add ChoiceField in forms and models

--- cowsay_app/forms.py
+++ cowsay_app/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+import subprocess
 
 
 class TextInput(forms.Form):
+    animals = subprocess.run(
+        ['cowsay', '-l'], capture_output=True).stdout.decode().split()[4:]
+    animals.insert(0, '---------')
+    choose_animal = forms.ChoiceField(choices=((x, x) for x in animals))
     text = forms.CharField(widget=forms.TextInput())
--- cowsay_app/models.py
+++ cowsay_app/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+import subprocess
 
 
 # Create your models here.
 class Main(models.Model):
+    animals = subprocess.run(
+        ['cowsay', '-l'], capture_output=True).stdout.decode().split()[4:]
+    choose_animal = models.CharField(
+        max_length=100, choices=((x, x) for x in animals))
     text = models.CharField(max_length=100)
     img = models.TextField()
 
